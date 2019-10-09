f0abce2eaae54088a6c26750e639100657371ab6
Willowsap <willowsapphire@protonmail.com>
2019-10-09 09:37:27 -0400

    cleaned up exercise16 js file

--- scripts/exercise16.js
+++ scripts/exercise16.js
@@ -1,16 +1,17 @@
+let current_date, hours, minutes, seconds, 
+    minutePad, secondPad;
 function updateTime() {
-    let current_date = new Date();
-    let hours = current_date.getHours();
-    let minutes = current_date.getMinutes();
-    let seconds = current_date.getSeconds();
-    let hourPad = hours < 10 ? "0" : "";
-    let minutePad = minutes < 10 ? "0" : "";
-    let secondPad = seconds < 10 ? "0" : "";
+    current_date = new Date();
+    hours = current_date.getHours();
+    minutes = current_date.getMinutes();
+    seconds = current_date.getSeconds();
+    minutePad = minutes < 10 ? "0" : "";
+    secondPad = seconds < 10 ? "0" : "";
 
-    th = document.getElementById("timeheader");
-    th.innerHTML = hourPad + hours + ":" 
-                 + minutePad + minutes + ":" 
-                 + secondPad + seconds;
-                 
+    document.getElementById("timeheader")
+        .innerHTML = hours + ":" 
+            + minutePad + minutes + ":" 
+            + secondPad + seconds;
+
     setTimeout("updateTime();", 1000);
 }
